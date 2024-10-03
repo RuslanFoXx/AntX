bbbe9ae0ed387806f619fc4005235d6bb03e4a43
FoXx <89214274+RuslanFoXx@users.noreply.github.com>
2024-10-03 09:37:23 +0300

    AntX/1.75 (x32)

--- AntX_64/resource.asm
+++ AntX_64/resource.asm
@@ -24,17 +24,17 @@ include 'thread\process.asm'
 szHeaderServer				DB 13,10, 'Server: AntX/1.75 x64'
 szVersionServer				DB 13,10, 'Date: '
 szHeaderType				DB ' GMT'
-							DB 13,10, 'Content-Type: '
+					DB 13,10, 'Content-Type: '
 szHeaderDisposition			DB 13,10, 'Content-Disposition: '
 szHeaderLength				DB 13,10, 'Content-Length: '
 szHeaderConnection			DB 13,10, 'Connection: '
-szClose						DB 'close'
-szKeepAlive					DB 'keep-alive'
+szClose					DB 'close'
+szKeepAlive				DB 'keep-alive'
 szKeepAliveEnd:
 ;------------------------------------------------
 ;   * * *  Init Const Strings  * * *
 ;------------------------------------------------
-szTagOk						DB  2, 'Ok'
+szTagOk					DB  2, 'Ok'
 szHeaderTextHtml			DB  9, 'text/html'
 ;------------------------------------------------
 ;   * * *  ConfigParamWords
@@ -52,7 +52,7 @@ sHexScaleChar				DB '0123456789ABCDEF'
 section '.idata' import data readable writeable
 
 DD 0,0,0,  RVA szKernel32,		RVA LibraryKernel32
-DD 0,0,0,  RVA szWinSocket2,	RVA LibraryWinSocket2
+DD 0,0,0,  RVA szWinSocket2,		RVA LibraryWinSocket2
 DD 0,0,0,  RVA szAdvAPI32,		RVA LibraryAdvAPI32
 DD 0,0,0,0,0
 ;------------------------------------------------
@@ -62,55 +62,55 @@ LibraryKernel32:
 
 GetLastError				DQ RVA szGetLastError
 VirtualAlloc				DQ RVA szVirtualAlloc
-VirtualFree					DQ RVA szVirtualFree
+VirtualFree				DQ RVA szVirtualFree
 GetTickCount				DQ RVA szGetTickCount
 GetLocalTime				DQ RVA szGetLocalTime
 GetSystemTime				DQ RVA szGetSystemTime 
 GetCommandLine				DQ RVA szGetCommandLine
-CreateIoCompletionPort		DQ RVA szCreateIoCompletionPort
-GetQueuedCompletionStatus	DQ RVA szGetQueuedCompletionStatus
-PostQueuedCompletionStatus	DQ RVA szPostQueuedCompletionStatus
-SetHandleInformation		DQ RVA szSetHandleInformation
-CloseHandle					DQ RVA szCloseHandle
-Sleep						DQ RVA szSleep
-CreateEvent					DQ RVA szCreateEvent
-SetEvent					DQ RVA szSetEvent
+CreateIoCompletionPort			DQ RVA szCreateIoCompletionPort
+GetQueuedCompletionStatus		DQ RVA szGetQueuedCompletionStatus
+PostQueuedCompletionStatus		DQ RVA szPostQueuedCompletionStatus
+SetHandleInformation			DQ RVA szSetHandleInformation
+CloseHandle				DQ RVA szCloseHandle
+Sleep					DQ RVA szSleep
+CreateEvent				DQ RVA szCreateEvent
+SetEvent				DQ RVA szSetEvent
 CreateThread				DQ RVA szCreateThread
-ExitThread					DQ RVA szExitThread
+ExitThread				DQ RVA szExitThread
 CreateProcess				DQ RVA szCreateProcess
-ExitProcess					DQ RVA szExitProcess
+ExitProcess				DQ RVA szExitProcess
 GetExitCodeProcess			DQ RVA szGetExitCodeProcess
-WaitForMultipleObjects		DQ RVA szWaitForMultipleObjects
+WaitForMultipleObjects			DQ RVA szWaitForMultipleObjects
 TerminateProcess			DQ RVA szTerminateProcess
-CreatePipe					DQ RVA szCreatePipe
-CreateFile					DQ RVA szCreateFile
-GetFileType					DQ RVA szGetFileType
+CreatePipe				DQ RVA szCreatePipe
+CreateFile				DQ RVA szCreateFile
+GetFileType				DQ RVA szGetFileType
 GetFileSizeEx				DQ RVA szGetFileSizeEx
-ReadFile					DQ RVA szReadFile
-WriteFile					DQ RVA szWriteFile
+ReadFile				DQ RVA szReadFile
+WriteFile				DQ RVA szWriteFile
 EndTableKernel32			DQ NULL
 ;------------------------------------------------
 ;   * * *  Import Table WinSocket2 * * *
 ;------------------------------------------------
 LibraryWinSocket2:
 
-setsockopt					DQ RVA szSetSockOpt
-bind						DQ RVA szBinding
-listen						DQ RVA szListen
-shutdown					DQ RVA szShutdown
-closesocket					DQ RVA szCloseSocket
-WSAStartup					DQ RVA szWSAStartup
+setsockopt				DQ RVA szSetSockOpt
+bind					DQ RVA szBinding
+listen					DQ RVA szListen
+shutdown				DQ RVA szShutdown
+closesocket				DQ RVA szCloseSocket
+WSAStartup				DQ RVA szWSAStartup
 WSAGetLastError				DQ RVA szWSAGetLastError
 WSACreateEvent				DQ RVA szWSACreateEvent
-WSAEnumNetworkEvents		DQ RVA szWSAEnumNetworkEvents
-WSAWaitForMultipleEvents	DQ RVA szWSAWaitForMultipleEvents
+WSAEnumNetworkEvents			DQ RVA szWSAEnumNetworkEvents
+WSAWaitForMultipleEvents		DQ RVA szWSAWaitForMultipleEvents
 WSAEventSelect				DQ RVA szWSAEventSelect
 WSACloseEvent				DQ RVA szWSACloseEvent
-WSASocket					DQ RVA szWSASocket
-WSAAccept					DQ RVA szWSAAccept
-WSASend						DQ RVA szWSASend
-WSARecv						DQ RVA szWSARecv
-WSACleanup					DQ RVA szWSACleanup
+WSASocket				DQ RVA szWSASocket
+WSAAccept				DQ RVA szWSAAccept
+WSASend					DQ RVA szWSASend
+WSARecv					DQ RVA szWSARecv
+WSACleanup				DQ RVA szWSACleanup
 EndTableWinSocket2			DQ NULL
 ;------------------------------------------------
 ;   * * *  Import Table AdvAPI32 * * *
@@ -118,8 +118,8 @@ EndTableWinSocket2			DQ NULL
 LibraryAdvAPI32:
 
 SetServiceStatus			DQ RVA szSetServiceStatus
-RegisterServiceCtrlHandler	DQ RVA szRegisterServiceCtrlHandler
-StartServiceCtrlDispatcher	DQ RVA szStartServiceCtrlDispatcher
+RegisterServiceCtrlHandler		DQ RVA szRegisterServiceCtrlHandler
+StartServiceCtrlDispatcher		DQ RVA szStartServiceCtrlDispatcher
 EndTableAdvAPI32			DQ NULL
 ;------------------------------------------------
 ;   * * *  Init Service Dispacher  * * *
@@ -129,8 +129,8 @@ SizeOfAddrIn				DQ SOCKADDR_IN_SIZE
 ;------------------------------------------------
 ;       * * *  WinAPI ProcNames
 ;------------------------------------------------
-szKernel32					DB 'KERNEL32.DLL',0
-szAdvAPI32					DB 'ADVAPI32.DLL',0
+szKernel32				DB 'KERNEL32.DLL',0
+szAdvAPI32				DB 'ADVAPI32.DLL',0
 szWinSocket2				DB 'WS2_32.DLL',0
 szServiceName				DB 'AntXServer',0
 
@@ -141,174 +141,174 @@ szGetTickCount				DB 0,0, 'GetTickCount',0
 szGetLocalTime				DB 0,0, 'GetLocalTime',0
 szGetSystemTime				DB 0,0, 'GetSystemTime',0
 szGetCommandLine			DB 0,0, 'GetCommandLineA',0
-szCreateIoCompletionPort	DB 0,0, 'CreateIoCompletionPort',0
-szGetQueuedCompletionStatus	DB 0,0, 'GetQueuedCompletionStatus',0
-szPostQueuedCompletionStatus	DB 0,0, 'PostQueuedCompletionStatus',0
-szSetHandleInformation		DB 0,0, 'SetHandleInformation',0
+szCreateIoCompletionPort		DB 0,0, 'CreateIoCompletionPort',0
+szGetQueuedCompletionStatus		DB 0,0, 'GetQueuedCompletionStatus',0
+szPostQueuedCompletionStatus		DB 0,0, 'PostQueuedCompletionStatus',0
+szSetHandleInformation			DB 0,0, 'SetHandleInformation',0
 szCloseHandle				DB 0,0, 'CloseHandle',0
-szSleep						DB 0,0, 'Sleep',0
+szSleep					DB 0,0, 'Sleep',0
 szCreateEvent				DB 0,0, 'CreateEventA',0
-szSetEvent					DB 0,0, 'SetEvent',0
+szSetEvent				DB 0,0, 'SetEvent',0
 szCreateThread				DB 0,0, 'CreateThread',0
 szExitThread				DB 0,0, 'ExitThread',0
 szCreateProcess				DB 0,0, 'CreateProcessA',0
 szExitProcess				DB 0,0, 'ExitProcess',0
-szGetExitCodeProcess		DB 0,0, 'GetExitCodeProcess',0
-szWaitForMultipleObjects	DB 0,0, 'WaitForMultipleObjects',0
+szGetExitCodeProcess			DB 0,0, 'GetExitCodeProcess',0
+szWaitForMultipleObjects		DB 0,0, 'WaitForMultipleObjects',0
 szTerminateProcess			DB 0,0, 'TerminateProcess',0
 szCreatePipe				DB 0,0, 'CreatePipe',0
 szCreateFile				DB 0,0, 'CreateFileA',0
 szGetFileType				DB 0,0, 'GetFileType',0
 szGetFileSizeEx				DB 0,0, 'GetFileSizeEx',0
-szReadFile					DB 0,0, 'ReadFile',0
-szWriteFile					DB 0,0, 'WriteFile',0
+szReadFile				DB 0,0, 'ReadFile',0
+szWriteFile				DB 0,0, 'WriteFile',0
 
 szSetSockOpt				DB 0,0, 'setsockopt',0
-szBinding					DB 0,0, 'bind',0
-szListen					DB 0,0, 'listen',0
-szShutdown					DB 0,0, 'shutdown',0
+szBinding				DB 0,0, 'bind',0
+szListen				DB 0,0, 'listen',0
+szShutdown				DB 0,0, 'shutdown',0
 szCloseSocket				DB 0,0, 'closesocket',0
 szWSAStartup				DB 0,0, 'WSAStartup',0
 szWSAGetLastError			DB 0,0, 'WSAGetLastError',0
 szWSACreateEvent			DB 0,0, 'WSACreateEvent',0
-szWSAEnumNetworkEvents		DB 0,0, 'WSAEnumNetworkEvents',0
-szWSAWaitForMultipleEvents	DB 0,0, 'WSAWaitForMultipleEvents',0
+szWSAEnumNetworkEvents			DB 0,0, 'WSAEnumNetworkEvents',0
+szWSAWaitForMultipleEvents		DB 0,0, 'WSAWaitForMultipleEvents',0
 szWSAEventSelect			DB 0,0, 'WSAEventSelect',0
 szWSACloseEvent				DB 0,0, 'WSACloseEvent',0
-szWSASocket					DB 0,0, 'WSASocketA',0
-szWSAAccept					DB 0,0, 'WSAAccept',0
-szWSASend					DB 0,0, 'WSASend',0
-szWSARecv					DB 0,0, 'WSARecv',0
+szWSASocket				DB 0,0, 'WSASocketA',0
+szWSAAccept				DB 0,0, 'WSAAccept',0
+szWSASend				DB 0,0, 'WSASend',0
+szWSARecv				DB 0,0, 'WSARecv',0
 szWSACleanup				DB 0,0, 'WSACleanup',0
 
-szSetServiceStatus				DB 0,0, 'SetServiceStatus',0
-szRegisterServiceCtrlHandler	DB 0,0, 'RegisterServiceCtrlHandlerA',0
-szStartServiceCtrlDispatcher	DB 0,0, 'StartServiceCtrlDispatcherA',0
+szSetServiceStatus			DB 0,0, 'SetServiceStatus',0
+szRegisterServiceCtrlHandler		DB 0,0, 'RegisterServiceCtrlHandlerA',0
+szStartServiceCtrlDispatcher		DB 0,0, 'StartServiceCtrlDispatcherA',0
 ;------------------------------------------------
 ;       * * *  Init Server Params  * * *
 ;------------------------------------------------
 section '.data' data readable writeable  ;  added 2 sections
 
-sStrByteScale	DD MAX_INT_SCALE dup(?)
-ServerTime		SYSTEMTIME ?
-LocalTime		SYSTEMTIME ?
+sStrByteScale				DD MAX_INT_SCALE dup(?)
+ServerTime				SYSTEMTIME ?
+LocalTime				SYSTEMTIME ?
 
-TotalHost		QWORD ?
-TotalProcess	QWORD ?
+TotalHost				QWORD ?
+TotalProcess				QWORD ?
 
-GetNetHost		LPNET_HOST ?
-SetRunProc		LPASK_EXT ?
-GetRunProc		LPASK_EXT ?
+GetNetHost				LPNET_HOST ?
+SetRunProc				LPASK_EXT ?
+GetRunProc				LPASK_EXT ?
 ;------------------------------------------------
 ;       * * *  Init Service DataSection
 ;------------------------------------------------
-ThreadServerCtrl	DWORD ?
-ThreadSocketCtrl	DWORD ?
-ThreadListenCtrl	DWORD ?
-ThreadProcessCtrl	DWORD ?
+ThreadServerCtrl			DWORD ?
+ThreadSocketCtrl			DWORD ?
+ThreadListenCtrl			DWORD ?
+ThreadProcessCtrl			DWORD ?
 
-hFile			HANDLE ?
-hFileReport		HANDLE ?
-hPortIOSocket	HANDLE ?
-hStatus			SERVICE_STATUS_HANDLE ?
+hFile					HANDLE ?
+hFileReport				HANDLE ?
+hPortIOSocket				HANDLE ?
+hStatus					SERVICE_STATUS_HANDLE ?
 
-SrvStatus		SERVICE_STATUS ?
-dSecurity		SECURITY_ATTRIBUTES ?
-Address			sockaddr_in ?
+SrvStatus				SERVICE_STATUS ?
+dSecurity				SECURITY_ATTRIBUTES ?
+Address					sockaddr_in ?
 ;------------------------------------------------
 ;       * * *  Init Config DataSection
 ;------------------------------------------------
-SocketDataSize		QWORD ?
-ServerResurseId		QWORD ?
-SetOptionPort		QWORD ?
-CountProcess		QWORD ?
-PostBytes			QWORD ?
-PipeBytes			QWORD ?
-TotalBytes			QWORD ?
-CountBytes			QWORD ?
-TransBytes			QWORD ?
-TransFlag			QWORD ?
+SocketDataSize				QWORD ?
+ServerResurseId				QWORD ?
+SetOptionPort				QWORD ?
+CountProcess				QWORD ?
+PostBytes				QWORD ?
+PipeBytes				QWORD ?
+TotalBytes				QWORD ?
+CountBytes				QWORD ?
+TransBytes				QWORD ?
+TransFlag				QWORD ?
 
-ServerConfig		SERVER_CONFIG ?
-lppTagRespont		RESPONT_HEADER ?
-lppReportMessages 	DD REPORT_MESSAGE_COUNT dup(?)
+ServerConfig				SERVER_CONFIG ?
+lppTagRespont				RESPONT_HEADER ?
+lppReportMessages 			DD REPORT_MESSAGE_COUNT dup(?)
 ;------------------------------------------------
 ;       * * *  Init Table Buffer
 ;------------------------------------------------
 GetMemoryBuffer:
-TabSocketIoData		LPPORT_IO_DATA ?
+TabSocketIoData				LPPORT_IO_DATA ?
 
-TabListenReport		LPREPORT_INFO ?
-GetListenReport		LPREPORT_INFO ?
-SetListenReport		LPREPORT_INFO ?
+TabListenReport				LPREPORT_INFO ?
+GetListenReport				LPREPORT_INFO ?
+SetListenReport				LPREPORT_INFO ?
 MaxListenReport:
 
-TabRouteReport		LPREPORT_INFO ?
-GetRouteReport		LPREPORT_INFO ?
-SetRouteReport		LPREPORT_INFO ?
+TabRouteReport				LPREPORT_INFO ?
+GetRouteReport				LPREPORT_INFO ?
+SetRouteReport				LPREPORT_INFO ?
 MaxRouteReport:
 
-TabQueuedProcess	LPPORT_IO_DATA ?
-GetQueuedProcess	LPPORT_IO_DATA ?
-SetQueuedProcess	LPPORT_IO_DATA ?
-MaxQueuedProcess	LPPORT_IO_DATA ?
+TabQueuedProcess			LPPORT_IO_DATA ?
+GetQueuedProcess			LPPORT_IO_DATA ?
+SetQueuedProcess			LPPORT_IO_DATA ?
+MaxQueuedProcess			LPPORT_IO_DATA ?
 ;------------------------------------------------
 ;       * * *  Init Report DataSection
 ;------------------------------------------------
-lpFileReport	LPREPORT_INFO ?
+lpFileReport				LPREPORT_INFO ?
 
-ListenReport	ACCEPT_HEADER ?
-RouterHeader	REPORT_HEADER ?
+ListenReport				ACCEPT_HEADER ?
+RouterHeader				REPORT_HEADER ?
 
-SystemReport	REPORT_INFO ?
-RouterReport	REPORT_INFO ?
-TimeOutReport	REPORT_INFO ?
+SystemReport				REPORT_INFO ?
+RouterReport				REPORT_INFO ?
+TimeOutReport				REPORT_INFO ?
 ;------------------------------------------------
 ;       * * *  Init Router DataSection
 ;------------------------------------------------
-lpPortIoCompletion	LPVOID ?
-lpSocketIoData		LPPORT_IO_DATA ?
-TransferredBytes	QWORD ?
+lpPortIoCompletion			LPVOID ?
+lpSocketIoData				LPPORT_IO_DATA ?
+TransferredBytes			QWORD ?
 ;------------------------------------------------
 ;       * * *  Init Listener DataSection
 ;------------------------------------------------
-WSockVer		WSADATA ?
-ListenEvent		WSANETWORKEVENTS ?
+WSockVer				WSADATA ?
+ListenEvent				WSANETWORKEVENTS ?
 
-TabListenSocket	SOCKET   MAX_NET_HOST dup(?)
-TabNetEvent		WSAEVENT MAX_NET_HOST dup(?)
-TabNetHost		DB NET_HOST_SIZE * MAX_NET_HOST dup(?)
+TabListenSocket	SOCKET			MAX_NET_HOST dup(?)
+TabNetEvent				WSAEVENT MAX_NET_HOST dup(?)
+TabNetHost				DB NET_HOST_SIZE * MAX_NET_HOST dup(?)
 ;------------------------------------------------
 ;       * * *  Init Process DataSection
 ;------------------------------------------------
-hInPipe			HANDLE ?
-hOutPipe		HANDLE ?
+hInPipe					HANDLE ?
+hOutPipe				HANDLE ?
 
-StartRunInfo	STARTUPINFO ?
-ProcRunInfo		PROCESS_INFORMATION ?
+StartRunInfo				STARTUPINFO ?
+ProcRunInfo				PROCESS_INFORMATION ?
 
-ErrRunProcess	ASK_EXT ?
-DefRunProcess	ASK_EXT ?
-TabRunProcess	DB ASK_EXT_SIZE * MAX_RUN_PROC dup(?)
+ErrRunProcess				ASK_EXT ?
+DefRunProcess				ASK_EXT ?
+TabRunProcess				DB ASK_EXT_SIZE * MAX_RUN_PROC dup(?)
 
-RunProcessSocket	LPPORT_IO_DATA MAXIMUM_WAIT_OBJECTS dup(?)
-RunProcessEvent		HANDLE MAXIMUM_WAIT_OBJECTS dup(?)
+RunProcessSocket			LPPORT_IO_DATA MAXIMUM_WAIT_OBJECTS dup(?)
+RunProcessEvent				HANDLE MAXIMUM_WAIT_OBJECTS dup(?)
 ;------------------------------------------------
 ;       * * *  Init Buffers
 ;------------------------------------------------
 TabConfig:
-szProcPath		DB MAX_PATH_SIZE dup(?)
-szProcDir		DB MAX_PATH_SIZE dup(?)
-szCmdPath		DB MAX_PATH_SIZE dup(?)
+szProcPath				DB MAX_PATH_SIZE dup(?)
+szProcDir				DB MAX_PATH_SIZE dup(?)
+szCmdPath				DB MAX_PATH_SIZE dup(?)
 
-szFileName		DB MAX_PATH_SIZE dup(?)
-szMoveName		DB MAX_PATH_SIZE dup(?)
+szFileName				DB MAX_PATH_SIZE dup(?)
+szMoveName				DB MAX_PATH_SIZE dup(?)
 ;------------------------------------------------
 ;       * * *  Strings Buffer
 ;------------------------------------------------
-szReportName	DB MAX_PATH_SIZE dup(?)
-_DataBuffer_	DB CONFIG_BUFFER_SIZE dup(?)
-szTextReport	DB REPORT_BUFFER_SIZE dup(?)
+szReportName				DB MAX_PATH_SIZE dup(?)
+_DataBuffer_				DB CONFIG_BUFFER_SIZE dup(?)
+szTextReport				DB REPORT_BUFFER_SIZE dup(?)
 ;------------------------------------------------
 ;   * * *   END  * * *
 ;------------------------------------------------
